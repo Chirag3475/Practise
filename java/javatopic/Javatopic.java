dbc703c7bc80ae52f1aa722b160fa2c6cf74bc52
chirag dholakiya <chiragdholakiya143@gmail.com>
2023-02-15 04:12:48 +0530

    First Practice

---
+++ java/javatopic/Javatopic.java
@@ -0,0 +1,14 @@
+package javatopic;
+
+public class Javatopic{
+    public static void main (String[] args)
+    {
+        System.out.println("Java Topic");
+        System.out.println("Variable");
+        System.out.println("method");
+        System.out.println("package");
+        System.out.println("constatnts");
+
+    }
+
+}
---
+++ java/manual/Manualtopic.java
@@ -0,0 +1,15 @@
+package manual;
+
+public class Manualtopic {
+    public static void main(String[] args){
+        System.out.println("Software testing");
+        System.out.println("Retesting");
+        System.out.println("Unit testing");
+        System.out.println("Alpha testing");
+        System.out.println("Bita testing");
+        System.out.println("System testing");
+        System.out.println("Importance of testing");
+        System.out.println("Challenges of tester");
+
+    }
+}
